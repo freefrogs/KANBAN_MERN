8cd34ecda2b6b686bd122d87154d972924d53915
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2018-04-24 18:58:37 +0200

    Add actions for lanes

--- server/routes/lane.routes.js
+++ server/routes/lane.routes.js
@@ -3,5 +3,13 @@ import * as LaneController from '../controllers/lane.controller';
 
 const router = new Router();
 
+//get all lanes
+router.route('/lanes').get(LaneController.getLanes);
+
+//add new lane
+router.route('/lanes').post(LaneController.addLane);
+
+//delete lane by lane Id
+router.route('/lanes/:laneId').delete(LaneController.deleteLane);
 
 export default router;
