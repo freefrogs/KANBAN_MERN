a14b9f1be7172637565de68eaa3202885aa97ea3
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2018-04-25 19:57:00 +0200

    Add endpoint - adding note

--- server/controllers/note.controller.js
+++ server/controllers/note.controller.js
@@ -1,5 +1,35 @@
+import Lane from '../models/lane';
 import Note from '../models/note';
+import uuid from 'uuid';
+
 
 export function getSomething(req, res) {
   return res.status(200).end();
+}
+
+export function addNote(req, res) {
+	const {note, laneId} = req.body;
+
+	if (!note || !note.task || !laneId) {
+		res.status(400).end();
+	}
+
+	const newNote = new Note({
+		task: note.task,
+	});
+
+	newNote.id = uuid();
+	newNote.save((err, saved) => {
+		if (err) {
+			res.status(500).send(err);
+		}
+		Lane.findOne({id: laneId})
+			.then(lane => {
+				lane.notes.push(saved);
+				return lane.save();
+			})
+			.then(() => {
+				res.json(saved);
+			});
+	});
 }
